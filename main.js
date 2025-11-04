40b14771b6e1fa3b7ff89047f68e1454aa843595
Lucas Pereira <llucaspereirasilva07@gmail.com>
2025-11-04 14:25:09 -0300

    Aplicado validação para input que não forem textos e que o numéro seja menor que zero

--- main.js
+++ main.js
@@ -5,16 +5,21 @@ const form = document.getElementById("investment-form");
 function renderProgression(event) {
   event.preventDefault();
   const startingAmount = Number(
-    document.getElementById("starting-amount").value
+    document.getElementById("starting-amount").value.replace(",", ".")
   );
   const additionalContribution = Number(
-    document.getElementById("additional-contribution").value
+    document.getElementById("additional-contribution").value.replace(",", ".")
   );
   const timeAmount = Number(document.getElementById("time-amount").value);
   const timeAmountPeriod = document.getElementById("time-amount-period").value;
-  const returnRate = Number(document.getElementById("return-rate").value);
+  const returnRate = Number(
+    document.getElementById("return-rate").value
+  ).replace(",", ".");
   const returnRatePeriod = document.getElementById("evaluation-period").value;
-  const taxRate = Number(document.getElementById("tex-rate").value);
+  const taxRate = Number(document.getElementById("tex-rate").value).replace(
+    ",",
+    "."
+  );
 
   const returnsArray = generateReturnArray(
     startingAmount,
@@ -26,6 +31,33 @@ function renderProgression(event) {
   );
 
   console.log(returnsArray);
+}
+
+function validateInput(event) {
+  if (event.target.value === "") {
+    return;
+  }
+
+  const { parentElement } = event.target;
+  const grandParentElement = event.target.parentElement.parentElement;
+  const inputValue = event.target.value.replace(",", ".");
+
+  if (isNaN(inputValue) || Number(inputValue) <= 0) {
+    const errorTextElement = document.createElement("p");
+    errorTextElement.classList.add("text-red-500");
+    errorTextElement.innerText = "Insira um valor numérico e maior que zero";
+
+    parentElement.classList.add("border-2");
+    parentElement.classList.add("border-red-500");
+    parentElement.classList.add("rounded-lg");
+    grandParentElement.appendChild(errorTextElement);
+  }
+}
+
+for (const formElement of form) {
+  if (formElement.tagName === "INPUT" && formElement.hasAttribute("name")) {
+    formElement.addEventListener("blur", validateInput);
+  }
 }
 
 form.addEventListener("submit", renderProgression);
